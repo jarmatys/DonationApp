879f8927219bfd2770dd2e70ffd6760ab5ec35ea
Jarek Armatys <jarekarmatys@gmail.com>
2020-06-16 11:47:01 +0200

    create project structure and database connection

---
+++ DonationApp/Context/DonationContext.cs
@@ -0,0 +1,21 @@
+﻿using DonationApp.Models.Db;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonationApp.Context
+{
+    public class DonationContext : IdentityDbContext<UserApp>
+    {
+        public DonationContext(DbContextOptions<DonationContext> options) : base(options) { }
+
+        // Zaślepka na klasę bazową
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- DonationApp/Startup.cs
+++ DonationApp/Startup.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DonationApp.Context;
+using DonationApp.Models.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +17,36 @@ namespace DonationApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup()
+        {
+            var config = new ConfigurationBuilder();
+
+            config.AddJsonFile("appsettings.json");
+
+            Configuration = config.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddIdentity<UserApp, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 2;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+
+            }).AddEntityFrameworkStores<DonationContext>();
+
+            services.AddDbContext<DonationContext>(builder =>
+            {
+                builder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +57,13 @@ namespace DonationApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
