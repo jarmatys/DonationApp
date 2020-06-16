e14234a08c5cf942b7e9d34256d52f50e6e99e7d
Jarek Armatys <jarekarmatys@gmail.com>
2020-06-16 14:58:24 +0200

    prepare account controller + views

--- DonationApp/Controllers/AccountController.cs
+++ DonationApp/Controllers/AccountController.cs
@@ -2,15 +2,89 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DonationApp.Models.Db;
+using DonationApp.Models.Views;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DonationApp.Context
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        protected UserManager<UserApp> UserManager { get; }
+        protected SignInManager<UserApp> SignInManager { get; }
+
+        public AccountController(UserManager<UserApp> userManager, SignInManager<UserApp> signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            ViewBag.Error = false;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromForm]RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new UserApp
+                {
+                    Name = model.Name,
+                    UserName = model.Email,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                };
+
+                var result = await UserManager.CreateAsync(user, model.Password);   //sprawdzenie hasła
+
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, false);   //zalogowanie od razu po rejestracji
+                    return RedirectToAction("login", "account");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            ViewBag.Error = true;
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+
+                ModelState.AddModelError("", "Invalid login attempt!");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await SignInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
         }
     }
 }
---
+++ DonationApp/Models/Views/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonationApp.Models.Views
+{
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        [EmailAddress(ErrorMessage = "Wprowadzono błędny adres email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Password { get; set; }
+    }
+}
